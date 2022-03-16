40f7105bff1ad3fe6edbf797d4aeda42674e3234
Gustavo Barros <gubarros.machado@gmail.com>
2022-03-15 22:30:26 -0300

    tirei parte dos alunos

--- js/Script.js
+++ js/Script.js
@@ -1,3 +1,5 @@
-/* LÓGICA DA NAVBAR
-Ao scrollar a página o elemento o header (cabeçalho) é alterado para um tamanho menor */
+/* LÓGICA DA NAVBAR - Ao scrollar a página o elemento o header (cabeçalho) é alterado para um tamanho menor */
 
+const menuLinks = document.querySelectorAll('.cabecalho-menu a');
+
+console.log(menuLinks);
