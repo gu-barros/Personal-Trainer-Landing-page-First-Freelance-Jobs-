3fd8e321a2af54cc93fc91564e45bd5a134ec30f
Gustavo Barros <gubarros.machado@gmail.com>
2022-03-21 20:59:25 -0300

    resizing secs

--- js/Script.js
+++ js/Script.js
@@ -1,4 +1,12 @@
+// Decrease header when scroll down
+window.onscroll = function () { scrollFunction() };
 
-const menuLinks = document.querySelectorAll('.cabecalho-menu-item');
-
-console.log(menuLinks);
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("header-mod").style.height = "50px";
+        document.getElementById("header-logo").style.height = "35px";
+    } else {
+        document.getElementById("header-mod").style.height = "75px";
+        document.getElementById("header-logo").style.height = "41px";
+    }
+}
